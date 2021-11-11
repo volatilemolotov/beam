b550eb668f6687b487980cce558e4b71bb47e399
daria.malkova <daria.malkova@akvelon.com>
2021-11-11 18:03:19 +0300

    Update playground/backend/internal/cloud_bucket/precompiled_objects.go
    Co-authored-by: Artur Khanin <artur.khanin@akvelon.com>

--- playground/backend/internal/cloud_bucket/precompiled_objects.go
+++ playground/backend/internal/cloud_bucket/precompiled_objects.go
@@ -280,7 +280,7 @@ func isDir(path string) bool {
 	return path[len(path)-1] == os.PathSeparator
 }
 
-// getSdkName get category and sdk from the name of the file name (path)
+// getSdkName gets category and sdk from the filepath
 func getSdkName(path string) string {
 	sdkName := strings.Split(path, string(os.PathSeparator))[0] // the path of the form "sdkName/example/", where the first part is sdkName
 	return sdkName
