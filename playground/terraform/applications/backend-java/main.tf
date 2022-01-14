08a9b88ccb0e0f28c409fcd2ee19e1688fc1d421
Sergey Kalinin <sergey.kalinin@akvelon.com>
2022-01-14 15:36:10 +0000

    Fix frontend build and additional small fixes to terraform

--- playground/terraform/applications/backend-java/main.tf
+++ playground/terraform/applications/backend-java/main.tf
@@ -54,7 +54,6 @@ resource "google_app_engine_flexible_app_version" "backend_app" {
   env_variables = {
      CACHE_TYPE="${var.cache_type}"
      CACHE_ADDRESS="${data.terraform_remote_state.remote_state_memorystore.outputs.memorystore_host}:6379"
-#     CACHE_ADDRESS="${var.cache_address}:6379"
      NUM_PARALLEL_JOBS=10
      LAUNCH_SITE = "app_engine"
   }
