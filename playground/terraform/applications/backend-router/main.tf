b6d1c98434c618c5d8d16b1f8bd88c0483e889b0
akustov <akustov@diasoft.ru>
2022-01-24 17:39:21 +0300

    fix deploy router

--- playground/terraform/applications/backend-router/main.tf
+++ playground/terraform/applications/backend-router/main.tf
@@ -42,8 +42,8 @@ resource "google_app_engine_flexible_app_version" "backend_app_router" {
   }
 
   automatic_scaling {
-    max_total_instances = 5
-    min_total_instances = 1
+    max_total_instances = 3
+    min_total_instances = 2
     cool_down_period = "120s"
     cpu_utilization {
       target_utilization = 0.7
@@ -51,14 +51,17 @@ resource "google_app_engine_flexible_app_version" "backend_app_router" {
   }
 
   resources {
-    memory_gb = 8
-    cpu = 4
+    memory_gb = 16
+    cpu = 8
   }
 
   env_variables = {
-    CACHE_TYPE      = "${var.cache_type}"
-    CACHE_ADDRESS      = "${data.terraform_remote_state.remote_state_memorystore.outputs.memorystore_host}:6379"
-    NUM_PARALLEL_JOBS      = 30
+    CACHE_TYPE="${var.cache_type}"
+    CACHE_ADDRESS="${data.terraform_remote_state.remote_state_memorystore.outputs.memorystore_host}:6379"
+    NUM_PARALLEL_JOBS=30
+    LAUNCH_SITE = "app_engine"
+    PIPELINE_EXPIRATION_TIMEOUT = "5m"
+    KEY_EXPIRATION_TIME = "7m"
   }
 
   deployment {
