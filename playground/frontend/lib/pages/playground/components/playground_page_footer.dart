0d3dd6053f719fe31152e98b1c5b1c020183ecf7
Alexander <alexander.zhuravlev@akvelon.com>
2022-01-27 14:08:52 +0400

    Fixed frontend precommit issues

--- playground/frontend/lib/pages/playground/components/playground_page_footer.dart
+++ playground/frontend/lib/pages/playground/components/playground_page_footer.dart
@@ -24,7 +24,6 @@ import 'package:playground/constants/links.dart';
 import 'package:playground/constants/sizes.dart';
 import 'package:playground/modules/analytics/analytics_service.dart';
 import 'package:playground/pages/playground/components/feedback/playground_feedback.dart';
-import 'package:playground/pages/playground/components/playground_privacy_policy.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class PlaygroundPageFooter extends StatelessWidget {
