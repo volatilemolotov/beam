93eab2329d415f51a3ec7b58983e2271ae9e86f5
MakarkinSAkvelon <67736809+MakarkinSAkvelon@users.noreply.github.com>
2022-07-12 19:04:06 +0500

    Update main.tf

--- playground/terraform/applications/default/main.tf
+++ playground/terraform/applications/default/main.tf
@@ -20,7 +20,7 @@
 resource "google_app_engine_application" "app_playground" {
   project       = var.project_id
   location_id   = var.location
-  database_type = "CLOUD_DATASTORE_COMPABILITY"
+  database_type = "CLOUD_DATASTORE_COMPATIBILITY"
 }
 
 resource "google_project_service" "firestore" {
