6b69c3e316a8595e3948bdf973a833ae8226707f
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-10-12 14:16:26 -0700

    Updated

---
+++ learning/tour-of-beam/terraform/environments/prod/main.tf
@@ -0,0 +1,43 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+locals {
+  env = "prod"
+}
+
+provider "google" {
+  project = var.project_id
+  region = var.region
+}
+
+module "setup" {
+  source             = "../../modules/setup/"
+  project_id         = var.project_id
+  service_account_id = var.service_account_id
+}
+
+module "buckets" {
+  source        = "../../modules/buckets"
+  project_id    = var.project_id
+  name          = var.function_bucket_name
+  storage_class = var.function_bucket_storage_class
+  location      = var.function_bucket_location
+}
+
+module "cloud-functions" {
+  source = "../../modules/cloud-functions"
+  project_id = var.project_id
+  region = var.region
+}
