74332ed26545f60786046879347e7beda8041e9b
Alexey Inkin <alexey.inkin@akvelon.com>
2023-01-24 17:00:10 +0400

    Add missing files (#25131)

---
+++ playground/frontend/integration_test/common/examples.dart
@@ -0,0 +1,9 @@
+import 'package:playground_components/playground_components.dart';
+import 'package:playground_components_dev/playground_components_dev.dart';
+
+final defaultExamples = {
+  Sdk.java: javaMinimalWordCount,
+  Sdk.go: goMinimalWordCount,
+  Sdk.python: pythonWordCountWithMetrics,
+  // Sdk.scio: scioMinimalWordCount,
+};
