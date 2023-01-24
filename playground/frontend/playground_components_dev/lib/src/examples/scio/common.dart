6a85b10b49f7211546f59042db010e7093b65f70
Alexey Inkin <alexey.inkin@akvelon.com>
2023-01-24 16:57:52 +0400

    Add SCIO example descriptor (#25131)

---
+++ playground/frontend/playground_components_dev/lib/src/examples/scio/common.dart
@@ -0,0 +1,3 @@
+/// Commit in spotify/scio at which deployed examples match GitHub.
+/// Must be hardcoded because we do not control the Spotify repository.
+const spotifyScioRef = '72423169818a25b0cf9321dc24660a1252890a54';
