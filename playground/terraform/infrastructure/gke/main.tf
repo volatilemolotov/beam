1d885637b136e658e8256e00401e0dd7cdab43c3
MakarkinSAkvelon <67736809+MakarkinSAkvelon@users.noreply.github.com>
2023-03-17 02:59:47 +0500

    Update main.tf

--- playground/terraform/infrastructure/gke/main.tf
+++ playground/terraform/infrastructure/gke/main.tf
@@ -25,11 +25,6 @@ resource "google_container_cluster" "playground-gke" {
   network                    = var.network
   subnetwork                 = var.subnetwork
   remove_default_node_pool = true
-
-  dns_config {
-    cluster_dns              = "CLOUD_DNS"
-    cluster_dns_scope        = "CLUSTER_SCOPE"
-  }
 }
 
 resource "google_container_node_pool" "playground-node-pool" {
