c393f2dcc6d1547d09a6379037c9d36398774eea
vitaly.terentyev <vitaly.terentyev@akvelon.com>
2024-04-17 17:09:34 +0400

    Test KafkaIOST with 5M records and without Reshuffle

--- it/kafka/src/test/java/org/apache/beam/it/kafka/KafkaIOST.java
+++ it/kafka/src/test/java/org/apache/beam/it/kafka/KafkaIOST.java
@@ -251,7 +251,7 @@ public final class KafkaIOST extends IOStressTestBase {
               .apply(
                   "One input to multiple outputs",
                   ParDo.of(new MultiplierDoFn<>(startMultiplier, loadPeriods)))
-              .apply("Reshuffle fanout", Reshuffle.viaRandomKey())
+//              .apply("Reshuffle fanout", Reshuffle.viaRandomKey())
               .apply("Counting element", ParDo.of(new CountingFn<>(WRITE_ELEMENT_METRIC_NAME)));
     }
     source.apply(
