9c8ce0aaf32b7eee3e93c133e8d67d31730b64e4
Vlado Djerek <djerek.vlado6@gmail.com>
2024-10-01 11:58:30 +0200

    test with venv

--- playground/infrastructure/cloudbuild/playground_cd_examples.sh
+++ playground/infrastructure/cloudbuild/playground_cd_examples.sh
@@ -99,23 +99,14 @@ apt install -y apt-transport-https ca-certificates software-properties-common cu
 add-apt-repository -y ppa:deadsnakes/ppa > /dev/null 2>&1 && apt update > /dev/null 2>&1
 apt install -y python3.8 python3.8-distutils python3-pip > /dev/null 2>&1
 apt install -y --reinstall python3.8-distutils > /dev/null 2>&1
-
-whereis python3.8
-
-python3.8 -m venv tmpvenv
-source tmpvenv/bin/activate
-
-exit 0
-
-
-pip install --upgrade google-api-python-client > /dev/null 2>&1
+pip install --upgrade google-api-python-client --break-system-packages
 python3.8 -m pip install pip --upgrade > /dev/null 2>&1
 ln -s /usr/bin/python3.8 /usr/bin/python > /dev/null 2>&1
 apt install -y python3.8-venv > /dev/null 2>&1
 
 LogOutput "Installing Python packages from beam/playground/infrastructure/requirements.txt"
 cd $BEAM_ROOT_DIR
-pip install -r playground/infrastructure/requirements.txt
+pip install -r playground/infrastructure/requirements.txt --break-system-packages
 
 LogOutput "Looking for files changed by the merge commit $MERGE_COMMIT"
 git fetch origin $MERGE_COMMIT
